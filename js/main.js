9ca158a74a1b26ae688933804df0208628e9f5a5
don-bigdad <bogdan.maksimenko.2002@gmail.com>
2022-08-24 19:54:58 +0300

    script for show by category,fix gallery also

--- js/main.js
+++ js/main.js
@@ -72,3 +72,28 @@ var countDownFunction =setInterval(function() {
 },1000)
 
 
+    let list = document.querySelectorAll(".list");
+    let itembox = document.querySelectorAll(".item");
+
+    for (let i=0;i<list.length;i++){
+      list[i].addEventListener("click",function(){
+        for(let j=0;j<list.length;j++){
+          list[j].classList.remove("active");
+        }
+        this.classList.add("active");
+
+        let dataFilter = this.getAttribute("data-filter");
+        
+        for (let k=0;k<itembox.length;k++){
+          itembox[k].classList.remove("active");
+          itembox[k].classList.add("hide");
+
+          if (itembox[k].getAttribute("data-item")==dataFilter ||
+          dataFilter == "all"){
+            itembox[k].classList.remove("hide");
+            itembox[k].classList.add("active");
+          }
+        }
+      })
+
+    }
