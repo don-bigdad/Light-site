dfcd890141c1a3e1c804c3bc08fc3e88e5ea2201
don-bigdad <bogdan.maksimenko.2002@gmail.com>
2022-08-30 23:43:22 +0300

    pagination.Problem is no sorted by category when click on page num

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import *
 from .forms import UserFormQuestion
 
+from django.core.paginator import Paginator
 
 
 def base(request):
@@ -10,15 +11,23 @@ def base(request):
         if message.is_valid():
             message.save()
             return redirect("/")
-
     category = Category.objects.filter(is_visible=True)
+
+
     product = Product.objects.filter(is_visible=True)
+    p = Paginator(product,2)
+    page = request.GET.get("page")
+    prod_list = p.get_page(page)
+
+
+
     sale_item = SaleItem.objects.filter(is_visible=True)
     slider = CustomSlider.objects.all()
     form = UserFormQuestion()
     data = {
         "categories":category,
         "product":product,
+        "prod_list":prod_list,
         "sale_item":sale_item,
         "slider":slider,
         "form":form,
