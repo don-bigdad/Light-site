548e14217cd6345ccb3ae4c477bf0b585146221f
don-bigdad <bogdan.maksimenko.2002@gmail.com>
2022-11-01 02:05:38 +0200

    start work for succesful display picture on heroku

--- cart/models.py
+++ cart/models.py
@@ -7,7 +7,9 @@ class UserOrderForm(models.Model):
     name = models.CharField(max_length=40,db_index=True)
     phone = models.CharField(max_length=15,validators=[mobile_re,])
     is_processed = models.BooleanField(default=False)
+    email = models.CharField(max_length=400,auto_created=True)
     order = models.TextField(max_length=400,auto_created=True)
+
 
     def __str__(self):
         return self.name
